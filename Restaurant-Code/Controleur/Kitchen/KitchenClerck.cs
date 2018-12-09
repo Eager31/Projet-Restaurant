728c8e556998ce160a4616be05a0f4f7716eb433
Unknown <emilien.pons@viacesi.fr>
2018-12-09 17:29:24 +0100

    Add KitchenClerck Controler

--- Restaurant-Code/Controleur/Kitchen/KitchenClerck.cs
+++ Restaurant-Code/Controleur/Kitchen/KitchenClerck.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Controleur.Commun;
+using Modèle.Plonge;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,38 @@ using System.Threading.Tasks;
 
 namespace Controleur.Cuisine
 {
-    class KitchenClerck
+    public class KitchenClerck : Actor
     {
+        public KitchenClerck(string name) : base(name)
+        {
+            this.mapAct.Add("CleanKitchen", new CleanKitchen());
+            this.mapAct.Add("ChopVegetables", new ChopVegetables());
+            this.mapAct.Add("CleanKitchenware", new CleanKitchenware());
+            this.mapAct.Add("CleanTableware", new CleanTableware());
+        }
+        
+
+        public void Action(String choice, WashMachine washMachine, Object itemToWash)
+        {
+            switch (choice)
+            {
+                case "CleanKitchen":
+                    this.mapAct["CleanKitchen"].voidAct();
+                    break;
+                case "ChopVegetables":
+                    this.mapAct["CleanKitchen"].voidAct(washMachine,(QueueKitchenTools) itemToWash);
+                    break;
+                case "CleanKitchenware":
+                    this.mapAct["CleanKitchenware"].voidAct(washMachine, (QueueRoomStuff)itemToWash);
+                    break;
+                case "CheckClock":
+                    this.checkTime();
+                    break;
+                default:
+                    //Do nothing ?
+                    break;
+            }
+        }
+
     }
 }
